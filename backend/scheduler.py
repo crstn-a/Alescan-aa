c21dae06777c15d27fd9e92593978d4473db07e4
crstn-a <ab.cristina00@gmail.com>
2026-09-08 08:44:57 +0800

    change to 23 hour for scheduler

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -13,7 +13,7 @@ def start_scheduler():
     _scheduler.add_job(
         run_sync,
         trigger=CronTrigger(
-            hour=24,
+            hour=23,
             minute=0
         ),
         id="daily_price_sync",
